36cb4521aec405485c0240e032fb751d1e950c54
anuradha2511 <anuradhapatil251999@gmail.com>
2022-10-04 14:46:54 +0530

    First program execution in JS
    First program execution in JS

---
+++ assignment.js
@@ -0,0 +1,6 @@
+console.log("Anuradha Mohan Patil");
+console.log("My hobbies - 1.Cooking 2.Gardening 3.Travel");
+console.info("My strength - 1.Honesty and loyalty 2.Positive thinker 3.Teamwork");
+console.warn("My weakness -  1.Sensitive  2.Public speaking 3.Self-criticism");
+console.log(30+50);
+console.log(49*479); 
---
+++ demo.js
@@ -0,0 +1,5 @@
+console.log("Hello");
+console.log(" My name is Anuradha Patil");
+console.info(" This is info"); //info means information
+console.warn("I am not that bad guy"); // warn means warning
+console.error(" Opps failed to execute");
