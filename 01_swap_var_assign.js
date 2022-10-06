992adefff2d2d23b6eb0ef8dd315ef5b85ffe95f
anuradha2511 <anuradhapatil251999@gmail.com>
2022-10-06 21:15:35 +0530

    practice
    practice

--- 01_swap_var_assign.js
+++ 01_swap_var_assign.js
@@ -1,4 +1,4 @@
-
+console.log("1) ");
 var sweety ="Sweety";
 var cutie = "Cutie";
 console.log("Before swapping");
@@ -8,14 +8,17 @@ var temp = sweety;
 sweety=cutie;
 cutie = temp;
 console.log("After swapping");
-console.log(cutie, sweety);
+console.log(sweety, cutie);
 
-
+console.log("2) ---------------");
 var num1=100,num2=200,num3=300;
 console.log("Before Swapping");
 console.log(num1,num2,num3);
 
-var temp = num1 ,num1=num2 , num2=num3, num3=temp;
+var temp = num1 
+num1=num2 
+ num2=num3
+  num3=temp;
 console.log("After Swapping");
 console.log(num1,num2,num3);
 
--- 01_variable_assign.js
+++ 01_variable_assign.js
@@ -6,5 +6,27 @@ myFullName = "Anuradha Mohan Patil"; //Update
 console.log(myFullName);
 var myLove = "I love only Js";
 console.log(myLove);
-var firstName = "Anuradha"; lastName="Patil"; age = "23"; address="Karad"; pin_code="415539"; gender="Female"; is_married="No"; //variable declaration & initialization on same line
+var firstName = "Anuradha"; lastName="Patil"; age = "23"; address="Karad"; pin_code="415539"; gender="Female"; is_married="No"; //variable declaration & variable initialization on same line
 console.log(firstName, lastName, age, address, pin_code, gender, is_married);
+
+
+// var myQualification; //declaration
+// myQualification = "BCA"; //initialization
+// console.log( "MY Qualification=",myQualification);
+
+// var myQualification = "BCA";
+// console.log(myQualification); 
+
+var favoriteFestival; 
+favoriteFestival = "Dussehra";
+console.log(favoriteFestival);
+
+favoriteFestival = "Diwali";
+console.log( "My Favorite Festival:",favoriteFestival);
+
+var myCollege;
+console.log(myCollege);
+myCollege = "K.R.P college";
+console.log(myCollege);
+myCollege = "K.R.P Kanya college, Islampur";
+console.log( "My College Name:",myCollege);
