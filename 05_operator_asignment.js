ecf27ab65d37b98f2258362bdf3f6f8051bf2438
anuradha2511 <anuradhapatil251999@gmail.com>
2022-10-12 17:22:07 +0530

    operators
    operators

---
+++ 05_operator_asignment.js
@@ -0,0 +1,47 @@
+console.log("....................... 1 ..........................");
+var worldLengthSquare = function(word){
+var worldLength = word.length;
+return worldLength * worldLength;
+}
+console.log("Length square :",worldLengthSquare("JavaScript"));
+console.log("Length square :",worldLengthSquare("Google"));
+console.log("Length square:",worldLengthSquare("Developer"));
+
+console.log("....................... 2 .........................");
+var test = function(){
+
+   var stringLength = "I am Angular Developer".length;
+console.log("Given string length :",stringLength);
+
+var string = "I am Angular Developer".split(" ");
+var total_words = string.length;
+
+console.log("Total number words :", total_words);
+
+ var divide = stringLength / total_words;
+console.log("String length divided by total number :",divide);
+
+var multiple = stringLength * total_words;
+console.log ("String multiply by total words :",multiple);
+
+console.log("........................3 ............................");
+var string = function(str){
+var givenString = "I am Angular Developer";
+console.log("Given string :",givenString);
+return str.split("").reverse().join("");
+}
+
+stringReverse = string("I am Angular Developer");
+console.log("Reverse string :",stringReverse);
+
+
+
+
+
+// var str = "   Hey you are doing good, keep it up    ";
+// console.log(str);
+
+}
+test();
+
+
---
+++ 05_ternary_operator_assign.js
@@ -0,0 +1,35 @@
+console.log("--------------------- 1. ------------------------");
+var greater_number = function(num1, num2){
+var result = num1>num2 ? num1 :num2;
+console.log("Greatest number 10, -10 : ",result);
+
+}
+greater_number(10, -10);
+
+var greater_number = function(num1, num2){
+    var result = num1>num2 ? num1 : num2;
+    console.log("Greatest number 800, 899 : ", result);
+}
+greater_number(800, 899);
+
+console.log("------------------------- 2. -----------------------");
+var evenOdd = function( num1){
+    var result = num1%2==0 ? true : false;
+    return result;
+}
+console.log("number is Even or odd :",evenOdd(29));
+console.log("number is Even or odd :",evenOdd(44)); 
+console.log(" number is Even or odd :",evenOdd(0));
+console.log("number is Even or odd :",evenOdd(101));
+
+console.log("-----------------------3. -----------------------------");
+var show = function(value){
+   var str = value.length;
+var result = str%2==0 ? "EVEN" : "ODD";
+console.log("length of word is EVEN or ODD :",result);
+}
+ show("JavaScript");
+ show("developer");
+ show("Google");
+
+
