099e94d6a7bd7814adcd45b3eae1571d08eb26b9
anuradha2511 <anuradhapatil251999@gmail.com>
2022-11-01 08:54:34 +0530

    Array , Set and Map
    Array , Set and Map

--- 11_array_basics.js
+++ 11_array_basics.js
@@ -25,3 +25,13 @@ let otherArray = [...arrayOfNumbers];
 arrayOfNumbers[3]  = 44;
 console.table(arrayOfNumbers);
 console.table(otherArray);
+
+console.log("==================================================");
+
+var arrayOfNumbers = [1, 3, 7, 8, 9, 3, 7];
+console.log("========== Resize array========");
+console.log(arrayOfNumbers.length);
+arrayOfNumbers.length = 3;
+console.log(arrayOfNumbers.length);
+console.log(arrayOfNumbers);
+console.log("========== Resize array========");
